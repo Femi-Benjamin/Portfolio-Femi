d0161d99d8af7efc80b4d470d995b25e249e9bb7
Olufemi Benjamin <benjaminolufemi16@gmail.com>
2024-02-19 13:18:59 -0600

    V5

--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -1,26 +1,28 @@
-"use client"
-import React from 'react'
-import { Projects } from '../components/constants'
-import ProjectCard from '../components/Projectcard'
-
+"use client";
+import React from "react";
+import { Projects } from "../components/constants";
+import ProjectCard from "../components/Projectcard";
 
 const page = () => {
   return (
     <div
-    style={{backgroundImage: "url(/mountains.jpg)"}}
-     className='w-screen h-screen flex items-center justify-center bg-center bg-cover'>
-      <div className='grid grid-cols-2 gap-5 max-w-[90%] max-h-[90%]'>
+      style={{backgroundImage: "url(/mountains.jpg)"}}
+      // style={{ backgroundImage: "url(atombg-comp.webp" }}
+      //style={{ backgroundImage: "url(/bg-2.jpg)" }}
+      className="w-screen h-screen flex items-center justify-center bg-center bg-cover"
+    >
+      <div className="grid grid-cols-2 gap-5 max-w-[90%] max-h-[90%]">
         {Projects.map((project, index) => (
           <ProjectCard
-              key={index}
-              title={project.title}
-              text={project.text}
-              image={project.src}
+            key={index}
+            title={project.title}
+            text={project.text}
+            image={project.src}
           />
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
